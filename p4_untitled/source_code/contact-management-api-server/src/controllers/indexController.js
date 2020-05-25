08e3fbc1cd9c3d201b91525b737a4e71f28802ed
Vaisakh B <vaisakhb12@gmail.com>
2020-05-25 13:05:23 +0530

    Refactoring

--- p4_untitled/source_code/contact-management-api-server/src/controllers/indexController.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', function (req, res, next) {
-  res.json('Server up and running!');
-});
-
-module.exports = router;
