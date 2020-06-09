b97ed3b30754ee94ca7d3f41e19e67a1214fca24
Vaisakh B <vaisakhb12@gmail.com>
2020-06-09 06:50:36 +0530

    updated part 4 presentation

--- workspace/src/hooks.js
+++
@@ -1,7 +0,0 @@
-import { useState } from "react";
-
-export const useInput = (initialValue) => {
-  const [value, setValue] = useState(initialValue);
-
-  return [value, (e) => setValue(e.target.value), () => setValue(initialValue)];
-};
